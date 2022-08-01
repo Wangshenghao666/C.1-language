87d3fae2132b2dcb41180090995ed1ba0bb01d91
Wangshenghao666 <2877959358@qq.com>
2022-08-01 22:54:41 +0800

    AVL Tree

---
+++ AVLTree/AVLTree.h
@@ -0,0 +1,291 @@
+#pragma once
+
+
+template<class K, class V>
+struct AVLTreeNode
+{
+	AVLTreeNode<K, V>* _left;
+	AVLTreeNode<K, V>* _right;
+	AVLTreeNode<K, V>* _parent;
+
+	pair<K, V> _kv;
+
+	int _bf; // 平衡因子
+
+	AVLTreeNode(const pair<K, V>& kv)
+		:_left(nullptr)
+		, _right(nullptr)
+		, _parent(nullptr)
+		, _bf(0)
+		, _kv(kv)
+	{}
+};
+
+template<class K, class V>
+class AVLTree
+{
+	typedef AVLTreeNode<K, V> Node;
+public:
+	AVLTree()
+		:_root(nullptr)
+	{}
+
+	bool Insert(const pair<K, V>& kv)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(kv);
+			return true;
+		}
+
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_kv.first < kv.first)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		cur = new Node(kv);
+		if (parent->_kv.first < kv.first)
+		{
+			parent->_right = cur;
+			cur->_parent = parent;
+		}
+		else
+		{
+			parent->_left = cur;
+			cur->_parent = parent;
+		}
+
+		// 控制平衡
+		// 1、更新平衡因子 -- 新增节点到根节点的祖先路径
+		// 2、出现异常平衡因子，那么需要旋转平衡处理
+		while (parent)
+		{
+			if (cur == parent->_left)
+				parent->_bf--;
+			else
+				parent->_bf++;
+
+			if (parent->_bf == 0)
+			{
+				break;
+			}
+			else if (parent->_bf == 1 || parent->_bf == -1)
+			{
+				// 继续往上更新
+				cur = parent;
+				parent = parent->_parent;
+			}
+			else if (parent->_bf == 2 || parent->_bf == -2)
+			{
+				// 旋转处理
+				// 右单旋
+				if (parent->_bf == -2 && cur->_bf == -1)
+				{
+					RotateR(parent);
+				}
+				else if (parent->_bf == 2 && cur->_bf == 1) // 左单旋
+				{
+					RotateL(parent);
+				}
+				else if (parent->_bf == -2 && cur->_bf == 1)
+				{
+					RotateLR(parent);
+				}
+				else if (parent->_bf == 2 && cur->_bf == -1)
+				{
+					RotateRL(parent);
+				}
+				else
+				{
+					assert(false);
+				}
+
+				break;
+			}
+			else
+			{
+				// 说明插入更新平衡因子之前，树中平衡因子就有问题了
+				assert(false);
+			}
+		}
+
+		return true;
+	}
+
+	void RotateL(Node* parent)
+	{
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+
+		parent->_right = subRL;
+		if (subRL)
+		{
+			subRL->_parent = parent;
+		}
+
+		Node* parentParent = parent->_parent;
+		subR->_left = parent;
+		parent->_parent = subR;
+
+		if (_root == parent)
+		{
+			_root = subR;
+			subR->_parent = nullptr;
+		}
+		else
+		{
+			if (parentParent->_left == parent)
+				parentParent->_left = subR;
+			else
+				parentParent->_right = subR;
+			subR->_parent = parentParent;
+		}
+
+		subR->_bf = parent->_bf = 0;
+	}
+
+	void RotateR(Node* parent)
+	{
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+
+		parent->_left = subLR;
+		if (subLR)
+			subLR->_parent = parent;
+
+		Node* parentParent = parent->_parent;
+
+		subL->_right = parent;
+		parent->_parent = subL;
+
+		if (parent == _root)
+		{
+			_root = subL;
+			_root->_parent = nullptr;
+		}
+		else
+		{
+			if (parentParent->_left == parent)
+				parentParent->_left = subL;
+			else
+				parentParent->_right = subL;
+
+			subL->_parent = parentParent;
+		}
+
+		subL->_bf = parent->_bf = 0;
+	}
+
+	void RotateLR(Node* parent)
+	{
+		//先保存
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+		int bf = subLR->_bf;
+
+		RotateL(parent->_left);
+		RotateR(parent);
+
+		//旋转完成，把平衡因子情况填上
+		if (bf == -1)
+		{
+			parent->_bf = 1;
+			subR->_bf = 0;
+			subRL->_bf = 0;
+		}
+		else if (bf == 1)
+		{
+			parent->_bf = 0;
+			subR->_bf = -1;
+			subRL->_bf = 0;
+		}
+		else if (bf == 0)
+		{
+			parent->_bf = 0;
+			subR->_bf = 0;
+			subRL->_bf = 0;
+		}
+		else
+		{
+			assert(false);
+		}
+	}
+
+	void RotateRL(Node* parent)
+	{
+		//先保存更新以后节点加平衡因子
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+		int bf = subR->_bf;
+
+		RotateR(parent->_right);
+		RotateL(parent);
+
+		//旋转完成，把平衡因子情况填上
+		if (bf == 1)
+		{
+			parent->_bf = -1;
+			subR->_bf = 0;
+			subRL->_bf = 0;
+		}
+		else if (bf == -1)
+		{
+			parent->_bf = 0;
+			subR->_bf = 1;
+			subRL->_bf = 0;
+		}
+		else if (bf == 0)
+		{
+			parent->_bf = 0;
+			subR->_bf = 0;
+			subRL->_bf = 0;
+		}
+		else
+		{
+			assert(false);
+		}
+	}
+	//写一个中序
+	void InOrder()
+	{
+		_InOrder(_root);
+	}
+	void _InOrder(Node* root)
+	{
+		if (root == nullptr)
+		{
+			return;
+		}
+		_InOrder(root->_left);
+		cout << root->_kv.first << " : " << root->_kv.second << endl;
+		_InOrder(root->_right);
+	}
+private:
+	Node* _root;
+};
+
+void test()
+{
+	AVLTree<int, int> t;
+	int a[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+	for (auto e : a)
+	{
+		t.Insert(make_pair(e, e));
+	}
+	t.InOrder();
+}
---
+++ AVLTree/Test.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<assert.h>
+//#include<utility>
+using namespace std;
+#include"AVLTree.h"
+
+int main()
+{
+	test();
+	return 0;
+}
